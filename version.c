6d02dbdfa8901b943ff600f03a848266a68ea73c
newnix <newnix>
2021-01-15 03:59:47 +0000

    Minor Changes to Build Process, Version Update
      * Makefile.in
        - Make SANITIZERS default to printing violation messages and using ubsan
        - Reorganize macro definitions
        - Have targets utilize built-in POSIX macros
    
      * mksignal
        - Remove RCS revision tag
    
      * version.c
        - Shorten & simplify version string
        - Bump to version 0.0-alpha1 (0.0-a1)

--- version.c
+++ version.c
@@ -1,4 +1,4 @@
 /* version.c -- version number */
 #include "es.h"
-static const char id[] = "@(#)nxes version 0.0-alpha0 20201216";
+static const char id[] = "@(#)nxes version 0.0-a1";
 const char * const version = id + (sizeof("@(#)") - 1);
